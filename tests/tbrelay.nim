abcac27f2a228ad1b30929bf519f367b746f5675
Matt Haggard <haggardii@gmail.com>
2023-01-28 01:14:36 -0500

    Fix singleusermode

--- tests/tbrelay.nim
+++ tests/tbrelay.nim
@@ -50,7 +50,7 @@ when singleusermode:
     withinTmpDir:
       let relayurl = "http://127.0.0.1:9001/v1/relay"
       tlog "Starting relay ..."
-      let server = startRelaySingleUser(9001.Port, "127.0.0.1", "alice", "password")
+      let server = startRelaySingleUser("alice", "password", 9001.Port, "127.0.0.1")
       tlog "Generating keys ..."
       let akeys = genkeys()
       let bkeys = genkeys()
