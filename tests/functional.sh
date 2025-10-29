d16bc3a586e989a9a07f2fe0a5ff27da65abc6c5
Matt Haggard <haggardii@gmail.com>
2025-10-29 16:47:32 -0400

    Run functional tests

--- tests/functional.sh
+++ tests/functional.sh
@@ -1,19 +1,17 @@
 
-set -x
-rm brelay.sqlite
+set -xe
+rm brelay.sqlite || echo okay to fail
 
 PORT=9000
 nim c -o:/tmp/server2 src/server2.nim
+nim c -o:/tmp/sampleclient src/sampleclient.nim
 /tmp/server2 server &
 SERVERPID="$!"
 while ! nc -z 127.0.0.1 $PORT; do
   echo "Waiting for $PORT"
   sleep 1
 done
-nim r src/sampleclient.nim publishnote topic1 value1
-nim r src/sampleclient.nim publishnote topic1 value1 || echo failed on purpose
+/tmp/sampleclient tests
 RC=$?
-
-
 pkill -P "$SERVERPID"
 exit "$RC"
